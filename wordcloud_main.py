a96b300515e163ef6475e19f5b75560641bbe410
stephotee <143778270+stephotee@users.noreply.github.com>
2024-01-21 19:07:22 +0800

    Update wordcloud_main.py

--- wordcloud_main.py
+++ wordcloud_main.py
@@ -51,34 +51,14 @@ def generate_word_cloud(text, max_words, color_profile, text_color, background_c
     st.pyplot()
 
 
-# Streamlit UI v1 start
-#st.title("Word Cloud Generator")
-
-# Text input
-#user_input = st.text_area("Enter your text here:")
-
-# Word count slider
-#max_words = st.slider("Max words in cloud", 5, 100, 50, 5)
-
-# Color profile selection
-#color_profiles = {
- #   "Black text, white background": ("#000000", "#FFFFFF"),
-  #  "White text, black background": ("#FFFFFF", "#000000"),
-   # "Multi-colour text, white background": ("", "#FFFFFF"),
-    #"Multi-colour text, black background": ("", "#000000")
-# }
-
-# Streamlit UI v1 end
-
 # Streamlit UI
 st.title("Word Cloud Generator")
 user_input = st.text_area("Paste your text here:")
-max_words = st.number_input("Maximum number of words", min_value=1, max_value=100, value=50)
+max_words = st.slider("Max words in cloud", 5, 100, 50, 5)
 color_choice = st.color_picker("Choose the text color for the word cloud", '#000000')
-group_terms_input = st.text_area("Group common terms (e.g., GROUP=(\"battery\", \"long battery\", \"long battery life\") TO=\"battery\")")
-
 color_profile = st.selectbox("Select Color Profile", list(color_profiles.keys()))
 text_color, background_color = color_profiles[color_profile]
+group_terms_input = st.text_area("Group common terms (e.g., GROUP=(\"battery\", \"long battery\", \"long battery life\") TO=\"battery\")")
 
 # Stopword input
 new_stopwords = [word.strip() for word in st.text_input("Enter words to exclude (separate with commas):").split(',')]
